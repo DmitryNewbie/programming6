3def8948c878d87224b99951db1f1f76933018f4
Dmitry <zimzulka1@gmail.com>
2023-10-13 23:49:29 +0400

    first commit

---
+++ з1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите цифры");
+int [] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for(int i=0; i<array.Length; i++)
+{
+    if(array[i]>0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Цифры больше нуля {count} ");
---
+++ з2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите К1  ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b1  ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2  ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2  ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+int x = -(b1- b2)/(k1- k2);
+int y =k1*x+b1;
+
+
+Console.WriteLine($"Точка пересечения ({x} {y})");
